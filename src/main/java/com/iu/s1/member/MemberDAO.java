8e601c7c1a99b147057517437eb6335d96a4cd9e
YoungDHi <sm930219@naver.com>
2023-02-06 17:07:01 +0900

    fixed untracked files

--- src/main/java/com/iu/s1/member/MemberDAO.java
+++ src/main/java/com/iu/s1/member/MemberDAO.java
@@ -3,6 +3,8 @@ package com.iu.s1.member;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.iu.s1.util.DBConnection;
@@ -10,27 +12,12 @@ import com.iu.s1.util.DBConnection;
 @Repository
 public class MemberDAO {
 	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE = "com.iu.s1.member.MemberDAO.";
+	
 	public int memberJoin(MemberDTO memberDTO) throws Exception {
-		Connection connection = DBConnection.getConnection();
-		
-		String sql = "INSERT INTO MEMBER(MEMBER_ID, MEMBER_PASSWORD, MEMBER_NAME, "
-				+ "MEMBER_ADDRESS, MEMBER_PHONE, MEMBER_EMAIL) "
-				+ "VALUES (?, ?, ?, ?, ?, ?)";
-		
-		PreparedStatement st = connection.prepareStatement(sql);
-		
-		st.setString(1, memberDTO.getMemberId());
-		st.setString(2, memberDTO.getMemberPassword());
-		st.setString(3, memberDTO.getMemberName());
-		st.setString(4, memberDTO.getMemberAddress());
-		st.setString(5, memberDTO.getMemberPhone());
-		st.setString(6, memberDTO.getMemberEmail());
-		
-		int result = st.executeUpdate();
-		
-		DBConnection.disconnect(st, connection);
-		
-		return result;
+		return sqlSession.insert(NAMESPACE+"memberJoin", memberDTO);
 		
 		
 		
